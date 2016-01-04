bc8b7c7647e53276d4ebe663692472fc960dd07e
Prasanna Shevade <pshevade@gmail.com>
2016-01-04 03:02:42 -0800

    Can't seem to run in production mode. Removed the conditional that checked if PRODUCTION env variable was set in settings/__init__.py

--- nanoproblems/nanoproblems/settings/__init__.py
+++ nanoproblems/nanoproblems/settings/__init__.py
@@ -4,5 +4,5 @@ if os.environ.get('BASE') == 'True':
     from .base import *
 elif os.environ.get('LOCAL') == 'True':
     from .local import *
-elif os.environ.get('PRODUCTION') == 'True':
+else:
     from .production import *
