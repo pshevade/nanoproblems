240c9d50c6473afaae2834309499cbb408bf2f12
Prasanna Shevade <pshevade@gmail.com>
2016-01-04 01:42:37 -0800

    EB configuration process.

--- nanoproblems/config/openid_settings.py
+++ nanoproblems/config/openid_settings.py
@@ -1,3 +1,8 @@
+import os
 
-REALM_URL = 'http://localhost:8000/'
-RETURN_URL = 'http://localhost:8000/users/login'
+if os.environ.get('PRODUCTION') == 'True':
+    REALM_URL = 'http://nanoproblems-dev.elasticbeanstalk.com/'
+    RETURN_URL = 'http://nanoproblems-dev.elasticbeanstalk.com/login'
+else:
+    REALM_URL = 'http://localhost:8000/'
+    RETURN_URL = 'http://localhost:8000/users/login'
--- nanoproblems/nanoproblems/settings/__init__.py
+++ nanoproblems/nanoproblems/settings/__init__.py
@@ -4,3 +4,5 @@ if os.environ.get('BASE') == 'True':
     from .base import *
 elif os.environ.get('LOCAL') == 'True':
     from .local import *
+elif os.environ.get('PRODUCTION') == 'True':
+    from .production import *
